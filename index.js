c2ce99c2d5316c5966767637024be8ae787ecde2
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-12 16:59:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Code your solution in this file
 function findMatching(names, value) {
     for (const name of names){
+      console.log(name)
       if (name == value){
-        console.log(names)
+        
       }
-      
+
     }
 }
