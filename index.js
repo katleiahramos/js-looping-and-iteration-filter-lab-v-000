3efe046bf1693117eebf22ac4ae4bf742f711d66
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-12 17:01:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers, value) {
+    const newDrivers = []
     for (const name of drivers){
-
       if (name == value){
         console.log(value)
       }
