d574f77d70d7f388347ecde74849738f4553c35a
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-12 17:32:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ def fuzzyMatch(drivers, value) {
   const newDrivers = []
   for (const name of drivers){
     if( name.substring(0,2) == value.substring(0,2)){
-
+      newDrivers.push(name)
     }
 
   }
